a1cd9d8b60a65e5de6d85af72c2ab74f043fe224
BinaryTENSHi <binarytenshi@gmail.com>
2014-03-26 20:18:52 +0100

    remove window stuffs

--- KongouFileMover/Main.cpp
+++ KongouFileMover/Main.cpp
@@ -1,16 +1,5 @@
-/* Windowstyles */
-#define ISOLATION_AWARE_ENABLED 1
-#if defined _M_IX86
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='X86' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#elif defined _M_X64
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='amd64' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#else
-#pragma comment(linker,"/manifestdependency:\"type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
-#endif
-
 #include <windows.h>
 #include <shlwapi.h>
-#include <CommCtrl.h>
 
 #include "Logger.h"
 #include "Configuration.h"
@@ -18,16 +7,6 @@
 #include "Expression.h"
 
 void printUsage();
-bool initializeWindow(HINSTANCE);
-
-const LPCWSTR className = L"popupWindowClass";
-const LPCWSTR progressClassName = L"popupWindowProgressClass";
-
-HWND windowHandle;
-HWND progressHandle;
-WNDCLASSEX windowClass;
-
-LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam);
 
 INT WINAPI WinMain(
     _In_ HINSTANCE instance,
@@ -35,21 +14,6 @@ INT WINAPI WinMain(
          _In_ LPSTR commandline,
          _In_ INT showCommand)
 {
-    if (!initializeWindow(instance))
-        return 1;
-
-    ShowWindow(windowHandle, SW_SHOW);
-    UpdateWindow(windowHandle);
-
-    MSG msg;
-    while (GetMessage(&msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&msg);
-        DispatchMessage(&msg);
-    }
-
-    return 0;
-
     bool move = false;
     bool hasFile = false;
     int argcount;
@@ -254,104 +218,4 @@ void printUsage()
                    L"KongouFileMover.exe [-c:configfile] [-x] %filename%",
                    L"KongouFileMover usage",
                    MB_OK | MB_ICONINFORMATION);
-}
-
-bool initializeWindow(HINSTANCE instance)
-{
-    ZeroMemory(&windowClass, sizeof(WNDCLASSEX));
-    windowClass.cbSize = sizeof(WNDCLASSEX);
-    windowClass.style = CS_HREDRAW | CS_VREDRAW | CS_NOCLOSE;
-    windowClass.lpfnWndProc = WndProc;
-    windowClass.cbClsExtra = 0;
-    windowClass.cbWndExtra = 0;
-    windowClass.hInstance = instance;
-    windowClass.hIcon = LoadIcon(instance, MAKEINTRESOURCE(IDI_ICON_APPLICATION));
-    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
-    windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    windowClass.lpszMenuName = NULL;
-    windowClass.lpszClassName = className;
-    windowClass.hIconSm = LoadIcon(instance, MAKEINTRESOURCE(IDI_ICON_APPLICATION));
-
-    if (!RegisterClassEx(&windowClass))
-    {
-        MessageBox(NULL, L"Window Registration Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
-        return false;
-    }
-
-    RECT desktop;
-    const HWND hDesktop = GetDesktopWindow();
-    GetWindowRect(hDesktop, &desktop);
-
-    int x = desktop.right - 240;
-    int y = desktop.bottom - 120;
-
-    HWND taskbar = FindWindow(L"Shell_TrayWnd", L"");
-    if (taskbar != nullptr)
-    {
-        RECT rect;
-        GetWindowRect(taskbar, &rect);
-        x = rect.right - 240;
-        y = rect.top - 120;
-    }
-
-    windowHandle = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-                        className,
-                        L"KongouFileMover",
-                        WS_POPUP,
-                        x,
-                        y,
-                        240,
-                        120,
-                        NULL,
-                        NULL,
-                        instance,
-                        NULL);
-
-    if (windowHandle == nullptr)
-    {
-        MessageBox(NULL, L"Window Creation Failed!", L"Error!", MB_ICONEXCLAMATION | MB_OK);
-        return false;
-    }
-
-    RECT client_rectangle;
-    GetClientRect(windowHandle, &client_rectangle);
-    int width = client_rectangle.right - client_rectangle.left;
-    int height = client_rectangle.bottom - client_rectangle.top;
-
-    progressHandle = CreateWindowEx(WS_EX_WINDOWEDGE | WS_EX_CLIENTEDGE,
-                                                    PROGRESS_CLASS,
-                                                    NULL,
-                                                    WS_CHILD | WS_VISIBLE | PBS_SMOOTH,
-                                                    (width - 220) / 2,
-                                                    height - 25,
-                                                    220,
-                                                    20,
-                                                    windowHandle,
-                                                    NULL,
-                                                    instance,
-                                                    NULL);
-
-    SendMessage(progressHandle, PBM_SETRANGE, 0, (LPARAM)MAKELPARAM(0, 100));
-    SendMessage(progressHandle, PBM_SETPOS, 0, 50);
-    return true;
-}
-
-LRESULT CALLBACK WndProc(HWND window, UINT message, WPARAM wparam, LPARAM lparam)
-{
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return true;
-
-    case WM_DESTROY:
-    case WM_CLOSE:
-        PostQuitMessage(0);
-        break;
-
-    default:
-        return DefWindowProc(window, message, wparam, lparam);
-    }
-
-    return 0;
 }
